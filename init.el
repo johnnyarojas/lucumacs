926a7e4cd556137837e20e7b4fe198e48ca12b9b
johnnyarojas <johnny.rojas.me@gmail.com>
2026-03-19 12:14:45 -0400

    Org Roam and mu4e support added

--- init.el
+++ init.el
@@ -157,7 +157,7 @@
        ;;nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org :roam v2)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
@@ -182,7 +182,7 @@
        ;;zig               ; C, but simpler
 
        :email
-       (mu4e +org +gmail)
+       (mu4e +org)
        ;;notmuch
        ;;(wanderlust +gmail)
 
